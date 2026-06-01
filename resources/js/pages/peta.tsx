9dc2f75cc9bf94cac50e536476f32549cf58fdf8
VelvetS2 <aboutvelvety@gmail.com>
2026-06-01 22:24:06 +0700

    tampilanPeta

---
+++ resources/js/pages/peta.tsx
@@ -0,0 +1,51 @@
+import { Head } from '@inertiajs/react';
+import L from 'leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { peta } from '@/routes';
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: new URL(
+        'leaflet/dist/images/marker-icon-2x.png',
+        import.meta.url,
+    ).toString(),
+    iconUrl: new URL(
+        'leaflet/dist/images/marker-icon.png',
+        import.meta.url,
+    ).toString(),
+    shadowUrl: new URL(
+        'leaflet/dist/images/marker-shadow.png',
+        import.meta.url,
+    ).toString(),
+});
+
+export default function Peta() {
+    return (
+        <>
+            <Head title="Peta" />
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
+                    <MapContainer
+                        center={[-4.5, 105.4]}
+                        zoom={8}
+                        scrollWheelZoom
+                        className="absolute inset-0 size-full"
+                    >
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>
+                </div>
+            </div>
+        </>
+    );
+}
+
+Peta.layout = {
+    breadcrumbs: [
+        {
+            title: 'Peta',
+            href: peta(),
+        },
+    ],
+};
