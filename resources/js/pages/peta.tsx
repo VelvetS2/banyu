6a871db402b32a2051d27b856f14573f56af549d
VelvetS2 <aboutvelvety@gmail.com>
2026-06-02 09:27:15 +0700

    batasKecamatan

--- resources/js/pages/peta.tsx
+++ resources/js/pages/peta.tsx
@@ -1,6 +1,7 @@
 import { Head } from '@inertiajs/react';
 import L from 'leaflet';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { GeoJSON, MapContainer, TileLayer } from 'react-leaflet';
 import { peta } from '@/routes';
 
 L.Icon.Default.mergeOptions({
@@ -18,7 +19,52 @@ L.Icon.Default.mergeOptions({
     ).toString(),
 });
 
+type KecamatanProperties = {
+    id: string;
+    kecamatan: string;
+    kabupaten: string;
+};
+
+type KecamatanFeature = GeoJSON.Feature<GeoJSON.Geometry, KecamatanProperties>;
+type KecamatanCollection = GeoJSON.FeatureCollection<
+    GeoJSON.Geometry,
+    KecamatanProperties
+>;
+
+const BATAS_STYLE: L.StyleFunction = () => ({
+    color: '#6b7280',
+    weight: 1,
+    fillColor: 'transparent',
+    fillOpacity: 0,
+});
+
 export default function Peta() {
+    const [kecamatan, setKecamatan] = useState<KecamatanCollection | null>(
+        null,
+    );
+
+    useEffect(() => {
+        let cancelled = false;
+        fetch('/lampung_kecamatan.geojson')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Gagal memuat GeoJSON kecamatan');
+                }
+                return response.json();
+            })
+            .then((data: KecamatanCollection) => {
+                if (!cancelled) {
+                    setKecamatan(data);
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+        return () => {
+            cancelled = true;
+        };
+    }, []);
+
     return (
         <>
             <Head title="Peta" />
@@ -34,6 +80,13 @@ export default function Peta() {
                             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
+                        {kecamatan && (
+                            <GeoJSON
+                                key={kecamatan.features.length}
+                                data={kecamatan as unknown as GeoJSON.GeoJsonObject}
+                                style={BATAS_STYLE as L.StyleFunction}
+                            />
+                        )}
                     </MapContainer>
                 </div>
             </div>
